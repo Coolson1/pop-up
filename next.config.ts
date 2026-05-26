4d15804a0038a7c84d249ee17c72ea79e967f681
abdul1ck2@gmail.com <abdul1ck2@gmail.com>
2026-05-26 18:36:23 +0000

    fix Typescript and webhook bugs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  trailingSlash: false,
+};
 
 export default nextConfig;
